0dd88608a92449a02c56c5bb5db72bd11d454048
BXSS101 <akarawinb@hotmail.com>
2020-11-29 22:31:01 +0700

    Edit Lab4

--- DataStruct Grader/L4/L402.py
+++ DataStruct Grader/L4/L402.py
@@ -4,14 +4,13 @@ def iSort(ls):
     for i in range(1, len(ls)):
         iEle = ls[i]
         for j in range(i, -1, -1) :
+            counter = counter + 1
             if iEle < ls[j-1] and j > 0:
                 ls[j] = ls[j-11]
-                counter = counter + 1
             else :
                 ls[j] = iEle
-                #counter = counter + 1
                 break
-
+            
 print("\n *** Insertion sort ***") 
 input_string = input("Enter some numbers : ")
 A=[]
