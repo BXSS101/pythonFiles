20f721f723e9d3cf8b7cebd52604723032aa16aa
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2021-01-30 13:10:00 +0700

    Update HW#2.py

--- Prob & Stat/HW#2.py
+++ Prob & Stat/HW#2.py
@@ -88,8 +88,10 @@ print("============================================================")
 print("============================================================")
 
 #Open File and add Value to List
+# 1 # C:/Users/akara/Documents/GitHub/pythonFiles/Prob & Stat/thaitourism2.csv
+# 2 # C:/Users/HP/Documents/GitHub/pythonFiles/Prob & Stat/thaitourism2.csv
 if inPath == '' :
-    path = 'C:/Users/akara/Documents/GitHub/pythonFiles/Prob & Stat/thaitourism2.csv'
+    path = 'C:/Users/HP/Documents/GitHub/pythonFiles/Prob & Stat/thaitourism2.csv'
 else :
     path = inPath
 with open(path, 'r') as file:
