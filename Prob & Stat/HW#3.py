93e0decfbad94af88f394c762b2b222816cdba9e
Ackrawin B <63220854+BXSS101@users.noreply.github.com>
2021-02-20 00:23:23 +0700

    Add PDF Plot

--- Prob & Stat/HW#3.py
+++ Prob & Stat/HW#3.py
@@ -14,9 +14,13 @@ Github URL : https://github.com/BXSS101
 ################
 # Program part #
 ################
+from scipy.stats import norm
 import matplotlib.pyplot as pltA
 import csv
 
+lst14, lst15, lst16 = [], [], []
+chklst14, chklst15, chklst16 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0], [0]
+count = 0
 #Start Program
 print("============================================================")
 print("============================================================")
@@ -27,6 +31,7 @@ print("    Please Choose a File to Proceed . . . ")
 print("        *leave it blank to use default file path")
 print("Enter File Path : ", end='')
 inPath = input()
+'''
 #select to display only year or year with months
 print("        ↓↓↓ Select display mode to dispaly data ↓↓↓")
 print("          \'A\' : Dispaly Month & Year")
@@ -39,6 +44,7 @@ while True :
         print(" !!!!!!!!       INPUT AGAIN       !!!!!!!!")
     else :
         break
+'''
 print("============================================================")
 print("============================================================")
 
@@ -50,4 +56,47 @@ if inPath == '' :
 else :
     path = inPath
     
-#Add open file
+with open(path, 'r') as file:
+    reader = csv.reader(file)
+    #process for all year
+    for row in reader:
+        #sum all year
+        if count >= 1 :
+            lst14.append(int(row[2]))
+            lst15.append(int(row[3]))
+            lst16.append(int(row[4]))
+            if int(row[2]) > 0 and int(row[2]) < 100001 :
+                chklst14[0] = chklst14[0] + 1
+            elif int(row[2]) > 100000 and int(row[2]) < 200001 :
+                chklst14[1] = chklst14[1] + 1
+            elif int(row[2]) > 200000 and int(row[2]) < 300001 :
+                chklst14[2] = chklst14[2] + 1
+            elif int(row[2]) > 300000 and int(row[2]) < 400001 :
+                chklst14[3] = chklst14[3] + 1
+            elif int(row[2]) > 400000 and int(row[2]) < 500001 :
+                chklst14[4] = chklst14[4] + 1
+            elif int(row[2]) > 500000 and int(row[2]) < 600001 :
+                chklst14[5] = chklst14[5] + 1
+            elif int(row[2]) > 600000 and int(row[2]) < 700001 :
+                chklst14[6] = chklst14[6] + 1
+            elif int(row[2]) > 900000 and int(row[2]) < 800001 :
+                chklst14[7] = chklst14[7] + 1
+            elif int(row[2]) > 800000 and int(row[2]) < 900001 :
+                chklst14[8] = chklst14[8] + 1
+            elif int(row[2]) > 900000 and int(row[2]) < 1000001 :
+                chklst14[9] = chklst14[9] + 1
+            elif int(row[2]) > 1000000 and int(row[2]) < 1100001 :
+                chklst14[10] = chklst14[10] + 1
+            elif int(row[2]) > 1100000 and int(row[2]) < 1200001 :
+                chklst14[11] = chklst14[11] + 1
+        count = count + 1
+
+lst14.sort()
+lst15.sort()
+lst16.sort()
+pltA.plot(lst15,norm.pdf(lst15))
+pltA.title('2015 Tourist')
+pltA.show()
+
+pltA.plot(lst16,norm.pdf(lst16))
+pltA.show()
--- Prob & Stat/playground.py
+++ Prob & Stat/playground.py
@@ -1,16 +1,4 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Create data
-N = 500
-x = np.random.rand(N)
-y = np.random.rand(N)
-colors = (0,0,0)
-area = np.pi*3
-
-# Plot
-plt.scatter(x, y, s=area, c=colors, alpha=0.5)
-plt.title('Scatter plot pythonspot.com')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+print(np.arange(-4,4,0.001))
